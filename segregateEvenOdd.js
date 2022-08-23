de14d56bc0704ed42afe6e5718a1e8c4eecfe6b9
CAdev1234 <fenglong37@outlook.com>
2022-08-22 23:48:28 -0400

    minimum moves in segregate even odd numbers: updated

--- segregateEvenOdd.js
+++ segregateEvenOdd.js
@@ -1,6 +1,5 @@
-// Minumum moves to separate even and odd numbers in array
-
 function segregateEvenOdd3(arr) {
+    // [...even, ...odd]
     let res = 0,
         left = 0,
         right = arr.length - 1;
@@ -25,6 +24,35 @@ function segregateEvenOdd3(arr) {
 
     return res;
 }
-var arr = [12, 34, 45, 9, 8, 90, 3];
+
+function segregateEvenOdd4(arr) {
+    // [...odd, ...even]
+    let res = 0,
+        left = 0,
+        right = arr.length - 1;
+    // two-pointer approach
+    while (left < right) {
+        if (arr[left] % 2 == 0) {
+            while (right > left && arr[right] % 2 == 0) {
+                // Find the first occurrence on the righthand side that can be swapped
+                right--;
+            }
+
+            if (right > left) {
+                // if we're already in the midpoint and the left pointer is odd then there is no swap
+                res++;
+                // handled this rightmost occurrence that was even => adjust to account for this
+                right--;
+            }
+        }
+
+        left++;
+    }
+
+    return res;
+}
+// var arr = [12, 34, 45, 9, 8, 90, 3];
+var arr = [3, 7, 4, 6, 7, 8, 3];
 let result = segregateEvenOdd3(arr);
-console.log(result)
+let result1 = segregateEvenOdd4(arr);
+console.log(`[...even, ...odd]=${result}, [...odd, ...even]=${result1}`)
